862728705ed77779f7a3e88d49ef34565410d10c
Sep Bar <sepand.barkhodaee@gmail.com>
2014-02-14 09:55:35 -0500

    arrays.js

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -7,4 +7,24 @@
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
 
+var newArr = ["basketball", "football", "volleyball"]
 
+console.log("My 1st choice is " + newArr[0])
+
+var choice = function(array) {
+	for(count= 0; count <= array.length-1; count++){
+		if (count == 0) {
+			 "My 1st choice is " + array[count]
+
+		}
+		else if (count == 1) {
+			 "My 2nd choice is " + array[count]
+		}
+		else {
+			 "My 3rd choice is " + array[count]
+		}
+	}
+
+}
+
+choice(newArr)
