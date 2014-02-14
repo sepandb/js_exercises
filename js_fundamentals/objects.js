d27bdfd4ec4b8f14e452e36fcfa845b0e988f4b5
Sep Bar <sepand.barkhodaee@gmail.com>
2014-02-14 14:44:02 -0500

    working on objects.js

--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -12,7 +12,18 @@
 // cinnamon
 // cumin
 // cocoa
+var recipe = {
+	name: "Mole",
+	serves: 2,
+	ingredients: ["cinnamon", "cumin", "cocoa"]
+}
 
+console.log(recipe.name)
+console.log("Serves: " + recipe.serves)
+console.log("Ingredients:")
+for (var i = 0; i <= recipe.ingredients.length; i++) {
+	console.log(recipe.ingredients[i])
+}
 // EXERCISE: The Reading List
 
 // Keep track of which books you read and which books you want to read!
@@ -20,6 +31,37 @@
 // Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
 // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+
+var books = [
+		{
+			'title': "MakkaBook",
+			'author': "Michael Jordan",
+			'alreadyRead': true
+		},
+		{
+			'title': "Elegant Universe",
+			'author': "Bison Green",
+			'alreadyRead': false
+		},
+		{
+			'title': "Hunger Games",
+			'author': "Lebron James",
+			'alreadyRead': true
+		}
+ ]
+
+ var output = function (book) {
+ 	for (var i = 0; i < book.length; i++) {
+ 		console.log(book[i]['title'] + " by " + book[i].author);
+ 		if (book[i].alreadyRead === true){
+ 			console.log("You already read " + book[i]['title']);
+ 		}
+ 		else {
+ 			console.log("YOU GOTTA READ " + book[i]['title']);
+ 		}
+ 	}
+}
+
 
 // EXERCISE: The Movie Database
 
