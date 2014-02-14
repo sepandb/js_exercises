f00bce6ec3a01645a56396aae0783ba4c25cbcc1
Sep Bar <sepand.barkhodaee@gmail.com>
2014-02-14 18:39:19 -0500

    completed objects.js

--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -40,7 +40,7 @@ var books = [
 		},
 		{
 			'title': "Elegant Universe",
-			'author': "Bison Green",
+			'author': "Brian Green",
 			'alreadyRead': false
 		},
 		{
@@ -69,6 +69,21 @@ var books = [
 
 // Create an object to store the following information about your favorite movie: title (a string), duration (a number), and stars (an array of strings).
 // Create a function to print out the movie information like so: "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
+
+var movie = {
+	'title': "The Dark Knight",
+	'duration': 130,
+	'stars': ["Bale", "Ledger", "Batman"]
+}
+
+
+
+var print = function(movie){
+	console.log(movie.title + " lasts for " + movie.duration + " minutes. Stars: " + movie.stars.join(", ") + ".")
+
+}
+
+print(movie)
 
 
 
